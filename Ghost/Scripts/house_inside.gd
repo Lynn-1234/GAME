36bda98ce0b931936deb0d3b56173fb5a2766e4b
Lynn-1234 <lylin4777@gmail.com>
2024-06-17 00:01:59 +1200

    chatbox

--- Ghost/Scripts/house_inside.gd
+++ Ghost/Scripts/house_inside.gd
@@ -10,15 +10,14 @@ func _ready():
 func spawn_player():
 	var player = PlayerSpawn.instantiate()
 	player.global_position = $Marker2D.global_position
-	
 	add_child(player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	#pass
 
 
 func _on_outside_body_entered(body):
-	if body.name == "Player":
+	if body.name == "Player": 
 		get_tree().change_scene_to_file("res://Scripts/main.tscn")
 	
--- Ghost/Scripts/npc_1.gd
+++ Ghost/Scripts/npc_1.gd
@@ -1,4 +1,6 @@
-extends RigidBody2D
+extends CharacterBody2D
+@export var playerPosition:CharacterBody2D
+var IsDialog = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +10,27 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if IsDialog:
+		if Input.is_action_just_pressed("dialogic_default_action"):
+			if Dialogic.current_timeline != null:
+				return
+			Dialogic.timeline_ended.connect(_on_timeline_ended)
+			var layput = Dialogic.start("NPC_chiaki_timeline")
+			get_viewport().set_input_as_handled()
+			pass
+	#move_and_slide()
+
+func _on_show_dialogue_body_entered(body): 
+	if body.name == "Player": 
+		$IcoSp.visible= true
+		$IcoAn.play("Default")
+		IsDialog = true
+
+func _on_timeline_ended():
+	IsDialog = false
+
+func _on_show_dialogue_body_exited(body):
+	if body.name == "Player":
+		$IcoSp.visible = false
+		$IcoAn.stop()
+		IsDialog = false
