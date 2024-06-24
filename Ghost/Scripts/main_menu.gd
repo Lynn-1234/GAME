ef66b63e0d0abd0194eb11460b6600d622f93ddf
Lynn-1234 <lylin4777@gmail.com>
2024-06-24 13:06:22 +1200

    button

--- Ghost/Scripts/main_menu.gd
+++ Ghost/Scripts/main_menu.gd
@@ -3,8 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#var start_button = $StartButton
-	#start_button.connect("pressed", Callable(self, "_on_start_button_pressed"))
+	var start_button = $StartButton
+	start_button.connect("pressed", Callable(self, "_on_start_button_pressed"))
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +13,10 @@ func _process(delta):
 
 
 func _on_start_button_pressed():
-	print("Start button pressed")
+	#var main_scene = load("res://Scripts/main.tscn")
+	#get_tree().change_scene_to_file("res://Scripts/main.tscn")
+	ScenceChange.switch_scene("res://Scripts/main.tscn")
+
+
+func _on_quite_button_pressed():
+	get_tree().quit()
