c847dbfedc00c5d2b65af1ed9f99bf3bc48b0e2b
Lynn-1234 <lylin4777@gmail.com>
2024-10-22 21:20:14 +1300

    comments

--- Ghost/Scripts/globals.gd
+++ Ghost/Scripts/globals.gd
@@ -3,22 +3,3 @@ var last_Player
 var is_first_time = true
 var can_move = true
 
-#var previous_scene_path = ""  # 用于存储上一个场景路径
-#
-## 保存当前场景路径
-#func save_previous_scene():
-	#if get_tree().current_scene != null:
-		#previous_scene_path = get_tree().current_scene.resource_path
-	#else:
-		#print("No current scene found!")
-#
-## 切换到给定路径的场景
-#func change_scene_to(scene_path):
-	#get_tree().change_scene(scene_path)
-#
-## 返回到上一个场景
-#func return_to_previous_scene():
-	#if previous_scene_path != "":
-		#change_scene_to(previous_scene_path)
-	#else:
-		#print("No previous scene saved.")
--- Ghost/Scripts/scence_change.gd
+++ Ghost/Scripts/scence_change.gd
@@ -10,22 +10,22 @@ func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() - 1)
 
-#场景切换函数,第一个参数表示切换到对应场景的路径，参数二表示人公出现的位置
+#Scene switching function
 func switch_scene(res_path):
 	call_deferred("_deferred_switch_scene", res_path)
-	#在空闲时调用该对象的_deferred_switch_scene方法。始终返回 null，不返回该方法的结果。
+	#Calls the object's _deferred_switch_scene method when idle.
 
-#真正切换场景函数,第一个参数表示切换到对应场景的路径，参数二表示人公出现的位置
+#The actual scene switching function
 func _deferred_switch_scene(res_path):
 	animation_player.play("fadin")
 	await animation_player.animation_finished
 	if current_scene != null:
-		current_scene.free()#释放当前场景
+		current_scene.free()#Release the current scene
 		
-	var scene = load(res_path)#载入新场景
+	var scene = load(res_path)#Load a new scene
 	current_scene = scene.instantiate()
-	get_tree().root.add_child(current_scene)#将场景添加到根目录
-	get_tree().current_scene = current_scene#当前场景设置为新的场景
+	get_tree().root.add_child(current_scene)#Add the scene to the root directory
+	get_tree().current_scene = current_scene#Set the current scene to the new scene
 	
 	animation_player.play_backwards("fadin")
 
